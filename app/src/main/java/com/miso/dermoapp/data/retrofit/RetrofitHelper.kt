992b20dccd2a5efe11adb8ce6e4c0c38f4b0546c
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-01-29 13:36:19 -0500

    Se implementa la capa de data en la aplicación

---
+++ app/src/main/java/com/miso/dermoapp/data/retrofit/RetrofitHelper.kt
@@ -0,0 +1,11 @@
+package com.miso.dermoapp.data.retrofit
+
+/****
+ * Project: DermoApp
+ * From: com.miso.dermoapp.data.retrofit
+ * Created by Jhonnatan E. Zamudio P. on 29/01/2023 at 1:33 p. m.
+ * All rights reserved 2023.
+ ****/
+
+class RetrofitHelper {
+}
