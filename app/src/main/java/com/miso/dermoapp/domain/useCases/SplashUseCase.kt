c95d7e304ddd45188aea97e88d634eb73b1f7537
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-01-29 20:56:06 -0500

    Caso 1 Cuando consulta la tabla versión en base de datos y no existe ningún registro getAppVersión() debe devolver la versión de compilación (Verde)

--- app/src/main/java/com/miso/dermoapp/domain/useCases/SplashUseCase.kt
+++ app/src/main/java/com/miso/dermoapp/domain/useCases/SplashUseCase.kt
@@ -1,6 +1,9 @@
 package com.miso.dermoapp.domain.useCases
 
+import com.miso.dermoapp.BuildConfig
+import com.miso.dermoapp.data.attributes.version.entitie.Version
 import com.miso.dermoapp.data.attributes.version.repository.VersionRepository
+import java.util.*
 
 /****
  * Project: DermoApp
@@ -11,7 +14,21 @@ import com.miso.dermoapp.data.attributes.version.repository.VersionRepository
 
 class SplashUseCase(val versionRepository: VersionRepository) {
     suspend fun getAppVersion():String {
-        val version = versionRepository.queryLastVersionLocal()
-        return version.toString()
+        var versionName = ""
+        val version  = versionRepository.queryLastVersionLocal()
+        version.map { x ->
+            versionName = x.versionName
+        }
+        if (versionName.equals("")){
+            versionName = BuildConfig.VERSION_NAME
+            versionRepository.insertVersionLocal(
+                Version(
+                0,
+                BuildConfig.VERSION_CODE,
+                versionName,
+                Calendar.getInstance().time)
+            )
+        }
+        return "Versión " + versionName
     }
 }
--- app/src/test/java/com/miso/dermoapp/domain/useCases/SplashUseCaseTest.kt
+++ app/src/test/java/com/miso/dermoapp/domain/useCases/SplashUseCaseTest.kt
@@ -1,8 +1,22 @@
 package com.miso.dermoapp.domain.useCases
 
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.miso.dermoapp.BuildConfig
+import com.miso.dermoapp.data.attributes.version.datasource.VersionDataSourceLocal
+import com.miso.dermoapp.data.attributes.version.repository.VersionRepository
+import com.miso.dermoapp.data.room.DermoAppDB
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 /****
  * Project: DermoApp
@@ -10,13 +24,45 @@ import org.junit.Assert.*
  * Created by Jhonnatan E. Zamudio P. on 29/01/2023 at 7:40 p. m.
  * All rights reserved 2023.
  */
-
+@RunWith(AndroidJUnit4::class)
+@Suppress("NonAsciiCharacters")
+@ExperimentalCoroutinesApi
+@Config(sdk = [30])
 class SplashScreenUseCaseTest {
-    val splashUseCase = SplashUseCase()
+    @OptIn(DelicateCoroutinesApi::class)
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+    private var context = ApplicationProvider.getApplicationContext<Context>()
+    private lateinit var database: DermoAppDB
+    private lateinit var versionDataSourceLocal: VersionDataSourceLocal
+    private lateinit var versionRepository: VersionRepository
+    private lateinit var splashUseCase: SplashUseCase
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            context,
+            DermoAppDB::class.java
+        ).allowMainThreadQueries().build()
+        versionDataSourceLocal = VersionDataSourceLocal.getInstance(database.versionDAO())
+        versionRepository = VersionRepository.getInstance(versionDataSourceLocal)
+        splashUseCase = SplashUseCase(versionRepository)
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
+        database.close()
+    }
 
     @Test
-    fun `Caso 01`() {
-        val result = splashUseCase.getAppVersion()
-        assertEquals("Versión " + BuildConfig.VERSION_NAME,result)
+    fun `Caso 01`(): Unit = runBlocking {
+        launch(Dispatchers.Main) {
+            val result = splashUseCase.getAppVersion()
+            assertEquals("Versión " + BuildConfig.VERSION_NAME, result)
+        }
     }
+
+
 }
