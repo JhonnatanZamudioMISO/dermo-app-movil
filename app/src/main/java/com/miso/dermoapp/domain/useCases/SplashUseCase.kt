13d1176d21bf3943b883665977676950deff11b6
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-02-02 14:44:18 -0500

    Caso 10 Cuando el usuario ya acepto la descarga y por alguna razon no termino el proceso shouldBeUpdate() debe devolver el valor de true (Verde)

--- app/src/main/java/com/miso/dermoapp/domain/useCases/SplashUseCase.kt
+++ app/src/main/java/com/miso/dermoapp/domain/useCases/SplashUseCase.kt
@@ -64,6 +64,8 @@ class SplashUseCase(val versionRepository: VersionRepository) {
         val appUpdateInfoTask = appUpdateManager.appUpdateInfo.result
         if (appUpdateInfoTask.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE) && appUpdateInfoTask.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE)
             isUpdate = true
+        else if (appUpdateInfoTask.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS)
+            isUpdate = true
         return isUpdate
     }
 }
