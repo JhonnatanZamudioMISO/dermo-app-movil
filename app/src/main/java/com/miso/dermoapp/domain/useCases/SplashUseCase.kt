259351d7c57a6e200b2f4f5b7ac8ac2e971bf823
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-02-02 14:46:37 -0500

    Caso 10 Refactor del método setUpdateAvailable (Azul)

--- app/src/main/java/com/miso/dermoapp/domain/useCases/SplashUseCase.kt
+++ app/src/main/java/com/miso/dermoapp/domain/useCases/SplashUseCase.kt
@@ -60,12 +60,9 @@ class SplashUseCase(val versionRepository: VersionRepository) {
     }
 
     fun shouldBeUpdated(appUpdateManager: AppUpdateManager): Boolean {
-        var isUpdate = false
         val appUpdateInfoTask = appUpdateManager.appUpdateInfo.result
-        if (appUpdateInfoTask.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE) && appUpdateInfoTask.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE)
-            isUpdate = true
-        else if (appUpdateInfoTask.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS)
-            isUpdate = true
-        return isUpdate
+        return (appUpdateInfoTask.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE) &&
+                appUpdateInfoTask.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) ||
+                (appUpdateInfoTask.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS)
     }
 }
