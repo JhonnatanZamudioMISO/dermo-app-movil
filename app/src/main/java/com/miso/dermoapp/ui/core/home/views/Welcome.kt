f9ac8e29983cecd49aaeb8efe6515bb92b7f148e
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-02-03 12:51:53 -0500

    Se implementa correctamente el redireccionamiento para que un paciente pueda crear su cuenta

--- app/src/main/java/com/miso/dermoapp/ui/core/home/views/Welcome.kt
+++ app/src/main/java/com/miso/dermoapp/ui/core/home/views/Welcome.kt
@@ -1,5 +1,6 @@
 package com.miso.dermoapp.ui.core.home.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -8,6 +9,7 @@ import com.miso.dermoapp.R
 import com.miso.dermoapp.databinding.ActivityWelcomeBinding
 import com.miso.dermoapp.ui.core.home.viewModels.WelcomeViewModel
 import com.miso.dermoapp.ui.core.home.viewModels.WelcomeViewModelFactory
+import com.miso.dermoapp.ui.core.session.views.SignUp
 import kotlinx.coroutines.DelicateCoroutinesApi
 
 class Welcome : AppCompatActivity() {
@@ -22,5 +24,12 @@ class Welcome : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome)
         binding.lifecycleOwner = this
         binding.vModel = viewModel
+        binding.ButtonCreateAccount.setOnClickListener { goToSignUp() }
+    }
+
+    private fun goToSignUp() {
+        val intent = Intent(this@Welcome, SignUp::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.left_in, R.anim.left_out)
     }
 }
