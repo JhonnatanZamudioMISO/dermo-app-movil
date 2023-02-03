492c19c0d9a72a890ba958cf7e8deb2208f9cb76
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-02-03 16:12:16 -0500

    Se implementan los elementos graficos relacionados con email y password

---
+++ app/src/main/java/com/miso/dermoapp/ui/core/session/viewModels/SignUpViewModel.kt
@@ -0,0 +1,11 @@
+package com.miso.dermoapp.ui.core.session.viewModels
+
+/****
+ * Project: DermoApp
+ * From: com.miso.dermoapp.ui.core.session.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 3/02/2023 at 3:17 p. m.
+ * All rights reserved 2023.
+ ****/
+
+class SignUpViewModel {
+}
