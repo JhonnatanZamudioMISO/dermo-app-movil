6155893725630248d2ba000efa6a098d708d4e2c
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-02-02 20:08:32 -0500

    Se implementa la interfaz grafica de la pantalla

---
+++ app/src/main/java/com/miso/dermoapp/ui/core/home/viewModels/SelectLanguageViewModel.kt
@@ -0,0 +1,11 @@
+package com.miso.dermoapp.ui.core.home.viewModels
+
+/****
+ * Project: DermoApp
+ * From: com.miso.dermoapp.ui.core.home.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 2/02/2023 at 7:33 p. m.
+ * All rights reserved 2023.
+ ****/
+
+class SelectLanguageViewModel {
+}
