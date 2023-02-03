bf33f4fec08406fbc97def69d62eea160d3336fd
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-02-02 20:20:25 -0500

    Se configura el viewModel

--- app/src/main/java/com/miso/dermoapp/ui/core/home/viewModels/SelectLanguageViewModel.kt
+++ app/src/main/java/com/miso/dermoapp/ui/core/home/viewModels/SelectLanguageViewModel.kt
@@ -1,5 +1,10 @@
 package com.miso.dermoapp.ui.core.home.viewModels
 
+import android.annotation.SuppressLint
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.DelicateCoroutinesApi
+
 /****
  * Project: DermoApp
  * From: com.miso.dermoapp.ui.core.home.viewModels
@@ -7,5 +12,24 @@ package com.miso.dermoapp.ui.core.home.viewModels
  * All rights reserved 2023.
  ****/
 
-class SelectLanguageViewModel {
+class SelectLanguageViewModel (): ViewModel() {
+
+}
+
+
+@SuppressLint("SelectLanguage")
+@DelicateCoroutinesApi
+@Suppress("UNCHECKED_CAST")
+class SelectLanguageViewModelFactory: ViewModelProvider.NewInstanceFactory() {
+    companion object {
+        @Volatile
+        private var instance: SelectLanguageViewModelFactory? = null
+        fun getInstance(): SelectLanguageViewModelFactory = instance ?: synchronized(this) {
+            instance ?: SelectLanguageViewModelFactory()
+        }
+    }
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return SelectLanguageViewModel() as T
+    }
 }
--- app/src/main/java/com/miso/dermoapp/ui/core/home/views/SelectLanguage.kt
+++ app/src/main/java/com/miso/dermoapp/ui/core/home/views/SelectLanguage.kt
@@ -2,11 +2,24 @@ package com.miso.dermoapp.ui.core.home.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.miso.dermoapp.R
+import com.miso.dermoapp.databinding.ActivitySelectLanguageBinding
+import com.miso.dermoapp.ui.core.home.viewModels.SelectLanguageViewModel
+import com.miso.dermoapp.ui.core.home.viewModels.SelectLanguageViewModelFactory
+import kotlinx.coroutines.DelicateCoroutinesApi
 
 class SelectLanguage : AppCompatActivity() {
+    private lateinit var viewModel: SelectLanguageViewModel
+    private lateinit var binding: ActivitySelectLanguageBinding
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select_language)
+        val viewModelFactory = SelectLanguageViewModelFactory.getInstance()
+        viewModel = ViewModelProvider(this, viewModelFactory)[SelectLanguageViewModel::class.java]
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_select_language)
+        binding.lifecycleOwner = this
+        binding.vModel = viewModel
     }
 }
