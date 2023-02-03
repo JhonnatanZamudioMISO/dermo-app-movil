e50c68c6cfee23d4179f67dad96be494339c828a
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-02-03 12:04:51 -0500

    Se implementa la pantalla Welcome

---
+++ app/src/main/java/com/miso/dermoapp/ui/core/home/viewModels/WelcomeViewModel.kt
@@ -0,0 +1,11 @@
+package com.miso.dermoapp.ui.core.home.viewModels
+
+/****
+ * Project: DermoApp
+ * From: com.miso.dermoapp.ui.core.home.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 3/02/2023 at 11:25 a. m.
+ * All rights reserved 2023.
+ ****/
+
+class WelcomeViewModel {
+}
