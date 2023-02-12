1d69cc86f6b6917f67d5045c4803ba3fc5e45f93
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-02-12 13:29:37 -0500

    En el formulario de iniciar sesión deben habilitarse los campos “Correo electrónico” y “Contraseña”.

---
+++ app/src/main/java/com/miso/dermoapp/ui/core/session/viewModels/LogInViewModel.kt
@@ -0,0 +1,11 @@
+package com.miso.dermoapp.ui.core.session.viewModels
+
+/****
+ * Project: DermoApp
+ * From: com.miso.dermoapp.ui.core.session.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 12/02/2023 at 1:20 p. m.
+ * All rights reserved 2023.
+ ****/
+
+class LogInViewModel {
+}
