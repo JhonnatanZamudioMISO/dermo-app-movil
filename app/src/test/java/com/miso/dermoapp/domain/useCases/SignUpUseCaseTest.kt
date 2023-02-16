04d86b28b6e0749d82fb2a45b876bbd54e8ec622
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-02-15 23:22:58 -0500

    Caso 03: Cuando el campo email es invalido, password es valido, confirmar passwors es invalido y no se ha aceptado los terminos el método changeEnableButto() debe ser igual a false

--- app/src/test/java/com/miso/dermoapp/domain/useCases/SignUpUseCaseTest.kt
+++ app/src/test/java/com/miso/dermoapp/domain/useCases/SignUpUseCaseTest.kt
@@ -58,4 +58,10 @@ class SignUpUseCaseTest {
         val result = signUpUseCase.changeEnableButton(0,0,0,true)
         Assert.assertEquals(false, result)
     }
+
+    @Test
+    fun `Caso 03`() {
+        val result = signUpUseCase.changeEnableButton(0,1,0,false)
+        Assert.assertEquals(false, result)
+    }
 }
