16c37ccbc5027bb4cc9e66dc006ed86f13beb71a
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-02-28 06:13:20 -0500

    Se implemento el formulario para la pantalla del perfil dermatologico

---
+++ app/src/main/java/com/miso/dermoapp/ui/core/profile/viewModels/UserDermatologicalProfileViewModel.kt
@@ -0,0 +1,13 @@
+package com.miso.dermoapp.ui.core.profile.viewModels
+
+import androidx.lifecycle.ViewModel
+
+/****
+ * Project: DermoApp
+ * From: com.miso.dermoapp.ui.core.profile.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 28/02/2023 at 4:42 a. m.
+ * All rights reserved 2023.
+ ****/
+
+class UserDermatologicalProfileViewModel: ViewModel() {
+}
