451b6195bc7a4fe7bfdc3b272d0669043b74133d
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-03-02 05:43:33 -0500

    Se implementa el viewModel con la vista

--- app/src/main/java/com/miso/dermoapp/ui/core/profile/views/UserDematologicalProfile.kt
+++ app/src/main/java/com/miso/dermoapp/ui/core/profile/views/UserDematologicalProfile.kt
@@ -2,11 +2,29 @@ package com.miso.dermoapp.ui.core.profile.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.miso.dermoapp.R
+import com.miso.dermoapp.databinding.ActivityUserDematologicalProfileBinding
+import com.miso.dermoapp.domain.models.enumerations.ResponseErrorField
+import com.miso.dermoapp.ui.core.profile.viewModels.UserDermatologicalProfileViewModel
+import com.miso.dermoapp.ui.core.profile.viewModels.UserDermatologicalProfileViewModelFactory
+import com.miso.dermoapp.ui.core.utils.LoadingDialog
+import kotlinx.coroutines.DelicateCoroutinesApi
 
 class UserDematologicalProfile : AppCompatActivity() {
+    private lateinit var viewModel: UserDermatologicalProfileViewModel
+    private lateinit var binding: ActivityUserDematologicalProfileBinding
+    private lateinit var loadingDialog: LoadingDialog
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_dematological_profile)
+        ResponseErrorField.initialize(applicationContext)
+        val viewModelFactory = UserDermatologicalProfileViewModelFactory.getInstance(this)
+        viewModel = ViewModelProvider(this, viewModelFactory)[UserDermatologicalProfileViewModel::class.java]
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_dematological_profile)
+        binding.lifecycleOwner = this
+        binding.vModel = viewModel
+        loadingDialog = LoadingDialog(this, getString(R.string.configurandoPerfilDermatologico))
     }
 }
