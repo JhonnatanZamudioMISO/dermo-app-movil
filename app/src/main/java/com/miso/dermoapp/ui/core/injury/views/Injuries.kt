a90cd41ef21d45b1bf163bc7a6edcd5c527e6d8d
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-03-12 00:30:25 -0500

    Se configura la vista con el ViewModel

--- app/src/main/java/com/miso/dermoapp/ui/core/injury/views/Injuries.kt
+++ app/src/main/java/com/miso/dermoapp/ui/core/injury/views/Injuries.kt
@@ -2,11 +2,24 @@ package com.miso.dermoapp.ui.core.injury.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.miso.dermoapp.R
+import com.miso.dermoapp.databinding.ActivityInjuriesBinding
+import com.miso.dermoapp.ui.core.injury.viewModels.InjuriesViewModel
+import com.miso.dermoapp.ui.core.injury.viewModels.InjuriesViewModelFactory
+import kotlinx.coroutines.DelicateCoroutinesApi
 
 class Injuries : AppCompatActivity() {
+    private lateinit var viewModel: InjuriesViewModel
+    private lateinit var binding: ActivityInjuriesBinding
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_injuries)
+        val viewModelFactory = InjuriesViewModelFactory.getInstance()
+        viewModel = ViewModelProvider(this, viewModelFactory)[InjuriesViewModel::class.java]
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_injuries)
+        binding.lifecycleOwner = this
+        binding.vModel = viewModel
     }
 }
