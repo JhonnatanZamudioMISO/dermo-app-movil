dbadac8cd64dc8b2e102b0f4439aa728bdfbc043
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-03-12 16:30:20 -0500

    Se implementa toda la interfaz gráfica para el tipo de lesión

---
+++ app/src/main/java/com/miso/dermoapp/ui/core/injury/viewModels/TypeOfInjuryViewModel.kt
@@ -0,0 +1,13 @@
+package com.miso.dermoapp.ui.core.injury.viewModels
+
+import androidx.lifecycle.ViewModel
+
+/****
+ * Project: DermoApp
+ * From: com.miso.dermoapp.ui.core.injury.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 12/03/2023 at 2:38 p. m.
+ * All rights reserved 2023.
+ ****/
+
+class TypeOfInjuryViewModel: ViewModel() {
+}
