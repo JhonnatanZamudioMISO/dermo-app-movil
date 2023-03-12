0e983af7abf071121f80e280c1a0092e25d5f663
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-03-11 23:51:16 -0500

    Se genera la interfaz gráfica para mostrar las lesiones

---
+++ app/src/main/java/com/miso/dermoapp/ui/core/injury/viewModels/InjuriesViewModel.kt
@@ -0,0 +1,13 @@
+package com.miso.dermoapp.ui.core.injury.viewModels
+
+import androidx.lifecycle.ViewModel
+
+/****
+ * Project: DermoApp
+ * From: com.miso.dermoapp.ui.core.injury.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 11/03/2023 at 9:35 p. m.
+ * All rights reserved 2023.
+ ****/
+
+class InjuriesViewModel: ViewModel() {
+}
