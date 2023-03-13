d8c7e36c795f44a45e309d80236553e85b3dfca8
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-03-12 19:46:08 -0500

    Se implementa la interfaz gráfica de la distribución de la lesión dermatologica

---
+++ app/src/main/java/com/miso/dermoapp/ui/core/injury/viewModels/DistributionViewModel.kt
@@ -0,0 +1,13 @@
+package com.miso.dermoapp.ui.core.injury.viewModels
+
+import androidx.lifecycle.ViewModel
+
+/****
+ * Project: DermoApp
+ * From: com.miso.dermoapp.ui.core.injury.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 12/03/2023 at 7:39 p. m.
+ * All rights reserved 2023.
+ ****/
+
+class DistributionViewModel: ViewModel() {
+}
