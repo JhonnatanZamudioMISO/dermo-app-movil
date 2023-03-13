ddba7e59a384c8f98b7b714b8c6a99c19e2755f8
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2023-03-12 19:51:38 -0500

    Se implementa la capa logica en Distribución de la lesión

--- app/src/main/java/com/miso/dermoapp/ui/core/injury/viewModels/DistributionViewModel.kt
+++ app/src/main/java/com/miso/dermoapp/ui/core/injury/viewModels/DistributionViewModel.kt
@@ -1,6 +1,12 @@
 package com.miso.dermoapp.ui.core.injury.viewModels
 
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.miso.dermoapp.R
+import com.miso.dermoapp.domain.useCases.DistributionUseCase
+import kotlinx.coroutines.DelicateCoroutinesApi
 
 /****
  * Project: DermoApp
@@ -10,4 +16,51 @@ import androidx.lifecycle.ViewModel
  ****/
 
 class DistributionViewModel: ViewModel() {
+    private var distributionData= MutableLiveData<String>()
+    val buttonContinueDrawable = MutableLiveData<Int>()
+    val buttonContinueEnable = MutableLiveData<Boolean>()
+    private val distributionUseCase = DistributionUseCase()
+    val navigateToPhotoOfInjury = MutableLiveData<Boolean>()
+
+    init {
+        navigateToPhotoOfInjury.value = false
+    }
+    fun setDistribution(context: Context, distribution: String, checked: Boolean) {
+        if (checked)
+            distributionData.value = distribution
+        else
+            distributionData.value = ""
+        distributionUseCase.saveDistribution(context,distribution)
+        changeEnableButton()
+    }
+
+    fun changeEnableButton() {
+        if (distributionData.value?.isNotEmpty() == true) {
+            buttonContinueDrawable.value = R.drawable.boton_oscuro
+            buttonContinueEnable.value = true
+        } else {
+            buttonContinueDrawable.value = R.drawable.boton_oscuro_disabled
+            buttonContinueEnable.value = false
+        }
+    }
+
+    fun Continue() {
+        navigateToPhotoOfInjury.value = true
+    }
+}
+
+@DelicateCoroutinesApi
+@Suppress("UNCHECKED_CAST")
+class DistributionViewModelFactory: ViewModelProvider.NewInstanceFactory() {
+    companion object {
+        @Volatile
+        private var instance: DistributionViewModelFactory? = null
+        fun getInstance(): DistributionViewModelFactory = instance ?: synchronized(this) {
+            instance ?: DistributionViewModelFactory()
+        }
+    }
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return DistributionViewModel() as T
+    }
 }
